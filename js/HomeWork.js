f1483e246cf64885d4e5c6b3d7cb5d9aaeca5648
VitaVasilieva <vita.sherban2016@gmail.com>
2025-02-02 12:22:58 +0100

    Add files via upload

---
+++ js/HomeWork.js
@@ -0,0 +1,166 @@
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах  1, 0, -3.
+
+// const a = -3;
+// if (a>0) {
+//     console.log ("true");
+// }
+// else if (a<0) {
+//     console.log ("false");
+// }
+
+// const a = 1;
+// if (a>0) {
+//     console.log ("true");
+// }
+// else if (a<0) {
+//     console.log ("false");
+// }
+
+// const a = 0;
+// if (a>0) {
+//     console.log ("true");
+// }
+// else if (a<0) {
+//     console.log ("false");
+// }
+
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах  'test', "qwerty", true
+
+// let a = "test";
+// let b = "test";
+// let c = "qwerty";
+
+// if (b) {
+//   console.log(true);
+// }
+
+//if (a) {
+// console.log(true);
+// }
+
+// if (c) {
+//     console.log(true);
+// }
+
+// Якщо змінна більше 10 -  відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах  1, 10, 13.
+
+// let num = 1;
+// if (num>10) num -= 5;
+// else if (num<10) num += 5;
+// console.log(num);
+
+// let num = 10;
+// if (num>10) num -= 5;
+// else if (num<10) num += 5;
+// console.log(num);
+
+// let num = 13;
+// if (num>10) num -= 5;
+// else if (num<10) num += 5;
+// console.log(num);
+
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+
+// const code = prompt("Введіть число від 1 до 12");
+// let mon;
+
+// switch (code) {
+//   case "1":
+//     mon = "January";
+//     alert(`${mon}`);
+//     break;
+
+//   case "2":
+//     mon = "February";
+//     alert(`${mon}`);
+//     break;
+
+//   case "3":
+//     mon = "March";
+//     alert(`${mon}`);
+//     break;
+
+//   case "4":
+//     mon = "April";
+//     alert(`${mon}`);
+//     break;
+
+//   case "5":
+//     mon = "May";
+//     alert(`${mon}`);
+//     break;
+
+//   case "6":
+//     mon = "June";
+//     alert(`${mon}`);
+//     break;
+
+//   case "7":
+//     mon = "July";
+//     alert(`${mon}`);
+//     break;
+
+//   case "8":
+//     mon = "August";
+//     alert(`${mon}`);
+//     break;
+
+//   case "9":
+//     mon = "September";
+//     alert(`${mon}`);
+//     break;
+
+//   case "10":
+//     mon = "October";
+//     alert(`${mon}`);
+//     break;
+
+//   case "11":
+//     mon = "November";
+//     alert(`${mon}`);
+//     break;
+
+//   case "12":
+//     mon = "December";
+//     alert(`${mon}`);
+//     break;
+// }
+
+//Зробіть сервіс який отримує тризначне число
+//Поверніть користувачу сумму цих чисел
+
+// const value = prompt ("Введіть трьохзначне число");
+// let a, b, c;
+//  a = value % 10;
+//  b = ((value % 100) - a) / 10;
+//  c = (value - (value % 100)) / 100;
+
+//  let sum = a+b+c;
+//  alert (`Сума цифр введеного числа: ${sum}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
